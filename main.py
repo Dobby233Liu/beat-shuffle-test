e25deb9000aa0f8320d099accd5c4f0db1d1f579
Liu Wenyuan <15816141883@163.com>
2021-10-01 00:17:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import a_cybers_world
 import sys
 
 x = 0
-
 try:
-    print(make_lemonade(a_cybers_world.song))
-except:
-    x = 1
-    raise
+    try:
+        print(make_lemonade(a_cybers_world.song))
+    except:
+        x = 1
+        raise
 finally:
     sys.exit(x)
