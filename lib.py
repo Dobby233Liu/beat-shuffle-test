9d7234b64ee99588ea51b518452825928c72b6e9
Liu Wenyuan <15816141883@163.com>
2021-09-30 23:44:13 +0800

    [skip ci]

--- lib.py
+++ lib.py
@@ -19,13 +19,37 @@ def s_to_ms(n):
 def each_beat_takes_seconds(bpm):
     return bpm / 60
 
+def guess_bpm(seg):
+    # reduce loudness of sounds over 120Hz (focus on bass drum, etc)
+    seg = seg.low_pass_filter(120.0)
+
+    # we'll call a beat: anything above average loudness
+    beat_loudness = seg.dBFS 
+
+    # the fastest tempo we'll allow is 240 bpm (60000ms / 240beats)
+    minimum_silence = int(60000 / 240.0)
+
+    nonsilent_times = pydub.silence.detect_nonsilent(seg, minimum_silence, beat_loudness)
+
+    spaces_between_beats = []
+    last_t = nonsilent_times[0][0]
+
+    for peak_start, _ in nonsilent_times[1:]:
+        spaces_between_beats.append(peak_start - last_t)
+        last_t = peak_start
+
+    # We'll base our guess on the median space between beats
+    spaces_between_beats = sorted(spaces_between_beats)
+    space = spaces_between_beats[len(spaces_between_beats) / 2]
+
+    return 60000 / space
+
 def arrange_like(origin, example):
     if len(origin) != len(example):
         raise Exception("no")
     ret = []
     for i in example:
         ret.append(origin[i])
-    print(str(ret), file=sys.stderr)
     return ret
 
 def shuffle_beats(songdata):
@@ -33,7 +57,7 @@ def shuffle_beats(songdata):
     new_aud = pydub.AudioSegment.empty()
 
     pat = get_random_beat_pattern()
-    slicing_portion = s_to_ms(each_beat_takes_seconds(songdata["bpm"]))
+    slicing_portion = s_to_ms(each_beat_takes_seconds(guess_bpm(origin_aud)))
     if len(origin_aud) % slicing_portion != 0: # add padding
         origin_seg = origin_aud + pydub.AudioSegment.silent(duration=(slicing_portion - (len(origin_aud) % slicing_portion)))
     rest_ms = len(origin_aud)
