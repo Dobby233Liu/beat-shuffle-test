ffd22affcf0b55de4853c61f477d7feda5ba6adc
Liu Wenyuan <15816141883@163.com>
2021-10-01 14:13:12 +0800

    Update lib.py

--- lib.py
+++ lib.py
@@ -3,7 +3,8 @@ import random
 import math
 import sys
 
-BEATS = 16
+BEATS = 128
+assert(BEATS % 4 == 0)
 
 def get_random_beat_pattern():
     ret = list(range(BEATS))
@@ -28,13 +29,19 @@ def arrange_like(origin, example):
 
     return ret
 
+def russian_roulette():
+    return random.randint(1, 6) == 4
+def chaos(seg):
+    if russian_roulette():
+        return seg.reverse()
+    return seg
+
 def shuffle_beats(songdata):
     buf = get_song_seg(songdata)
     new_aud = pydub.AudioSegment.empty()
 
     pat = get_random_beat_pattern()
     slicing_portion = s_to_ms(each_beat_takes_seconds(songdata["bpm"]))
-    print(pat)
     print(slicing_portion)
 
     if "start" in songdata:
@@ -49,19 +56,20 @@ def shuffle_beats(songdata):
             cutoff = slicing_portion
             seg = buf[:cutoff]
             buf = buf[cutoff:]
-            seg = seg.apply_gain(-seg.max_dBFS).remove_dc_offset()
+            seg = chaos(seg.apply_gain(-seg.max_dBFS).remove_dc_offset())
             segs.append(seg)
         segs = arrange_like(segs, pat)
         for part in segs:
-            new_aud = new_aud.append(part, crossfade=0)
+            new_aud = new_aud.append(chaos(part), crossfade=0)
+        pat = get_random_beat_pattern()
 
-    new_aud = new_aud.apply_gain(-new_aud.max_dBFS).remove_dc_offset()
+    new_aud = chaos(new_aud.apply_gain(-new_aud.max_dBFS).remove_dc_offset())
     assert(len(new_aud) == supposed_len)
 
-    return new_aud, [i + 1 for i in pat]
+    return new_aud
 
 def make_lemonade(songdata):
     out = shuffle_beats(songdata)
-    fn = "shuffled_" + ''.join([str(i) for i in out[1]]) + "_" + songdata["fn"]
-    out[0].export(fn, format=songdata["ff"])
+    fn = "shuffled_" + songdata["fn"]
+    out.export(fn, format=songdata["ff"])
     return fn
