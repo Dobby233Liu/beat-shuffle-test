a1ee290bf988cca1e215111d567baa118fa6accc
Liu Wenyuan <15816141883@163.com>
2021-10-01 22:15:39 +0800

    Update song.py

--- song.py
+++ song.py
@@ -3,4 +3,4 @@ song = {}
 song["fn"] = "spamton_neo_mix_ex_wip.ogg" # BIG SHOT
 song["ff"] = "ogg"
 
-song["bpm"] = 139.95 # beat per minute. odd value because odd song
+song["bpm"] = 140.2 # beat per minute. odd value because odd song
