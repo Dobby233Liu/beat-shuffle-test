25d0c6832fa5964194dee887e95f8198df2fcb8e
Liu Wenyuan <15816141883@163.com>
2021-10-01 22:40:08 +0800

    [skip ci]

--- song.py
+++ song.py
@@ -3,7 +3,7 @@ song = {}
 song["fn"] = "spamton_neo_mix_ex_wip.ogg" # BIG SHOT
 song["ff"] = "ogg"
 
-song["bpm"] = 140 # beat per minute
+song["bpm"] = 139.9 # beat per minute
 
 print(60/140)
 print(60/140.2)
