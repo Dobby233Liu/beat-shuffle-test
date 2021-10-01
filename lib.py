fa732c6ed8ca66865f4ca3bfacbcab7ff1efc1c4
Liu Wenyuan <15816141883@163.com>
2021-10-01 14:35:44 +0800

    Update lib.py

--- lib.py
+++ lib.py
@@ -6,8 +6,8 @@ import sys
 BEATS = 32
 assert(BEATS % 4 == 0)
 
-def get_random_beat_pattern():
-    ret = list(range(BEATS))
+def get_random_beat_pattern(beats=BEATS):
+    ret = list(range(beats))
     random.shuffle(ret)
     return ret
 
@@ -17,8 +17,8 @@ def get_song_seg(songdata):
 def s_to_ms(n):
     return math.floor(n * 1000)
 
-def each_beat_takes_seconds(bpm):
-    return round(60 / bpm / (BEATS / 4) * 1000) / 1000
+def each_beat_takes_seconds(bpm, beats=BEATS):
+    return round(60 / bpm / (beats / 4) * 1000) / 1000
 
 def arrange_like(origin, example):
     assert(len(origin) == len(example))
@@ -36,36 +36,43 @@ def chaos(seg):
         return seg.reverse()
     return seg
 
-def shuffle_beats(songdata):
-    buf = get_song_seg(songdata)
+def _shuffle_beats(songdata, songseg, beats=BEATS):
+    buf = songseg
     new_aud = pydub.AudioSegment.empty()
-
-    pat = get_random_beat_pattern()
-    slicing_portion = s_to_ms(each_beat_takes_seconds(songdata["bpm"]))
-    print(slicing_portion)
 
     if "start" in songdata:
         buf = buf[s_to_ms(songdata["start"]):]
     if "end" in songdata:
         buf = buf[:-s_to_ms(songdata["end"])]
     supposed_len = len(buf)
+ 
+    pat = get_random_beat_pattern(beats=beats)
+    slicing_portion = s_to_ms(each_beat_takes_seconds(songdata["bpm"], beats=beats))
 
     tot = 0
     while len(buf) > 0:
         tot = tot + 1
         segs = []
-        for beat in range(BEATS):
+        for beat in range(beats):
             cutoff = slicing_portion
             seg = buf[:cutoff]
             buf = buf[cutoff:]
             segs.append(seg)
         segs = arrange_like(segs, pat)
         for part in segs:
-            new_aud = new_aud.append((tot % (BEATS / 4) == 0 and chaos(part) or part), crossfade=0)
+            new_aud = new_aud.append((tot % (beats / 4) == 0 and chaos(part) or part), crossfade=0)
+        pat = get_random_beat_pattern(beats=beats)
 
     new_aud = chaos(new_aud)
 
     return new_aud
+def shuffle_beats(songdata):
+    songseg = get_song_seg(songdata)
+    beats = BEATS
+    while beats != 1:
+        songseg = _shuffle_beats(songdata, songseg, beats=beats)
+        beats = beats / 4
+    return songseg
 
 def make_lemonade(songdata):
     out = shuffle_beats(songdata)
