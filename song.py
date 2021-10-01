97ab435962a9d417fdfe88484dc4a66593f4314e
Liu Wenyuan <15816141883@163.com>
2021-10-01 22:04:51 +0800

    Update song.py

--- song.py
+++ song.py
@@ -3,4 +3,5 @@ song = {}
 song["fn"] = "spamton_neo_mix_ex_wip.ogg" # BIG SHOT
 song["ff"] = "ogg"
 
-song["bpm"] = 140.1 # beat per minute
+song["bpm"] = 140 # beat per minute
+song["beat_delay"] = 1/96
