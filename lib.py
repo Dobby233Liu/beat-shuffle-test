e9a0f63e7435997c03faa816a707eb78b0013b18
Liu Wenyuan <15816141883@163.com>
2021-10-01 11:30:15 +0800

    Update lib.py

--- lib.py
+++ lib.py
@@ -14,7 +14,7 @@ def get_song_seg(songdata):
     return pydub.AudioSegment.from_file(songdata["fn"], songdata["ff"])
 
 def s_to_ms(n):
-    return round(round(n * 1000) / 10) * 10
+    return round(n * 1000)
 
 def each_beat_takes_seconds(bpm):
     return 60 / bpm
@@ -33,6 +33,7 @@ def shuffle_beats(songdata):
 
     pat = get_random_beat_pattern()
     slicing_portion = s_to_ms(each_beat_takes_seconds(songdata["bpm"]))
+    print(pat)
     print(slicing_portion)
     #if len(origin_aud) % slicing_portion != 0: # add padding
     #    origin_aud = origin_aud + pydub.AudioSegment.silent(duration=(slicing_portion - (len(origin_aud) % slicing_portion)))
@@ -43,14 +44,16 @@ def shuffle_beats(songdata):
 
     while rest_ms > 0:
         segs = []
-        for _ in range(BEATS):
+        for beat in range(BEATS):
             start_seek = seek
             seek = seek + slicing_portion
             if (seek - start_seek) > rest_ms:
                 seek = rest_ms
                 rest_ms = 0
+                print("failsafe")
             else:
-                rest_ms = rest_ms - slicing_portion
+                rest_ms = rest_ms - (seek - start_seek)
+            print(str(beat) + ":" + "%d:%d, remainder %d" % (start_seek, seek, rest_ms))
             seg = origin_aud[start_seek:seek]
             seg = seg.apply_gain(-seg.max_dBFS)
             segs.append(seg)
