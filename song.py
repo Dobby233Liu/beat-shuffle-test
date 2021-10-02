2e4628860bd32ccfe707d4673dcd9360c996980e
Liu Wenyuan <15816141883@163.com>
2021-10-02 13:51:55 +0800

    Update song.py

--- song.py
+++ song.py
@@ -1,3 +1,5 @@
+import random
+
 song = {}
 
 song["fn"] = "spamton_neo_mix_ex_wip.ogg" # BIG SHOT
@@ -5,4 +7,9 @@ song["ff"] = "ogg"
 
 song["bpm"] = 140 # beat per minute
 
-song["new_order"] = [1, 1, 0, 2]
+order = [1, 2, 3, 4]
+def new_order():
+    random.shuffle(order)
+    return order, True
+
+song["new_order"] = new_order
