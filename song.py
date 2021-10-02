de0acaadc5cc023b786db61fa2e1d29534eb7afd
Liu Wenyuan <15816141883@163.com>
2021-10-02 15:40:11 +0800

    brainfuck

--- song.py
+++ song.py
@@ -7,11 +7,8 @@ song["ff"] = "ogg"
 
 song["bpm"] = 140 # beat per minute
 
-choices = [1, 2, 3, 4, 0]
-ord = [1, 2, 3, 4]
 def new_order(tick):
-    if tick % 2 == 0:
-        ord = [random.choice(choices) for _ in range(4)]
+    ord = [random.choice([1, 2, 3, 4, 0]) for _ in range(4)]
     return ord, True
 
 song["new_order"] = new_order
