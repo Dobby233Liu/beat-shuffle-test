3c103afd046d3a8287a5c547470a67e371f5d395
Liu Wenyuan <15816141883@163.com>
2021-10-04 18:28:03 +0800

    Update song.py

--- song.py
+++ song.py
@@ -2,10 +2,7 @@ import random
 
 song = {}
 
-song["fn"] = "snd_usefountain.ogg" # Smart Race
+song["fn"] = "GALLERY.ogg" # Smart Race
 song["ff"] = "ogg"
 
-song["bpm"] = 64 # beat per minute
-
-song["beats"] = 3
-song["new_order"] = [1, 3, 2]
+song["bpm"] = 129 # beat per minute
