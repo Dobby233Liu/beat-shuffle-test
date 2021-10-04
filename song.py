542e58ca97d40f9018b4e92941f75541545fe082
Liu Wenyuan <15816141883@163.com>
2021-10-04 18:02:16 +0800

    Update song.py

--- song.py
+++ song.py
@@ -2,14 +2,7 @@ import random
 
 song = {}
 
-song["fn"] = "berdly_chase.ogg" # Smart Race
+song["fn"] = "snd_usefountain.ogg" # Smart Race
 song["ff"] = "ogg"
 
-song["bpm"] = 150 # beat per minute
-
-def new_order(tick):
-    ret = [1, 4, 3, 2]
-    if tick % 9 > 4:
-        ret = [3, 2, 1, 4]
-    return ret, True
-song["new_order"] = new_order
+song["bpm"] = 62 # beat per minute
